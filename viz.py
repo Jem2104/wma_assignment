9dc2bbc971b6756aa6ccb965f73a763f46684552
Jem2104 <58264346+Jem2104@users.noreply.github.com>
2021-06-04 11:56:19 +0200

    added graph

--- viz.py
+++ viz.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('memphis_reviews_updated.csv')
 
@@ -8,11 +9,21 @@ df['length'] = df['text'].str.count(' ') + 1
 
 df = df[['length', 'rating']]
 df = df.groupby(['length'], as_index=False).mean()
-print(df.head())
 
-plt.plot(df['length'], df['rating'])
-plt.title('Average rating per length')
+# binning them boys
+bins = np.arange(0, 1000, 25)
+df = df.groupby(pd.cut(df['length'], bins=bins))['rating'].agg(['mean'])
+df['length'] = bins[:-1]
+
+x, y = df['length'], df['mean']
+plt.plot(x, y, 'o')
+
+# adding a regression line
+b, m = np.polyfit(x, y, 1)
+plt.plot(x, m*x + b, '-')
+
+plt.title('Average rating per binned length with steps of 25')
 plt.xlabel('Length of review')
 plt.ylabel('Average rating')
-plt.savefig('average_rating_per_length')
+# plt.savefig('average_rating_per_length_binned')
 plt.show()
